00b0cdae53ded7cc3ac6cbe6211e45fb30903a8c
Fagner Dev <fagnersro.dev@gmail.com>
2025-03-13 23:05:34 -0300

    Dynamically creating route parameters for resource ID - @Get(:id)

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,13 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('recados')
 export class RecadosController {
-  // Encontra todos os recados
+  @Get()
   findAll(): string {
     return 'Essa rota retorna todos os recados';
   }
 
-  // Encontra um recado
+  @Get(':id')
   findOne(): string {
     return 'Essa rota retorna UM recado';
   }
