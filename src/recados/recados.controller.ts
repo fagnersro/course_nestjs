6d0dc69db29ad3e420ba12e83c684efe9625d83b
Fagner Dev <fagnersro.dev@gmail.com>
2025-03-17 11:24:23 -0300

    Creating a message Entity and completing our in-memory DB service

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -18,33 +18,30 @@ export class RecadosController {
 
   @HttpCode(HttpStatus.OK)
   @Get()
-  findAll(@Query() pagination: any): string {
+  findAll(@Query() pagination: any) {
     const { limit = 10, offset = 0 } = pagination;
     // return `Essa rota retorna todos os recados. Limite = ${limit} OffSet = ${offset}.`;
-    return this.recadosService.hello();
+    return this.recadosService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     console.log(id);
-    return `Essa rota retorna o ID: ${id}`;
+    return this.recadosService.findOne(id);
   }
 
   @Post()
-  create(@Body() body: string) {
-    return body;
+  create(@Body() body: any) {
+    return this.recadosService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body: any) {
-    return {
-      id,
-      ...body,
-    };
+    return this.recadosService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `Essa rota apaga o recado ID ${id}`;
+    return this.recadosService.remove(id);
   }
 }
--- src/recados/recados.service.ts
+++ src/recados/recados.service.ts
@@ -1,8 +1,63 @@
 import { Injectable } from '@nestjs/common';
+import type { Recado } from './entities/recado.entity';
 
 @Injectable()
 export class RecadosService {
-  hello(): string {
-    return 'Hello World!';
+  private lastId = 1;
+  private recados: Recado[] = [
+    {
+      id: 1,
+      texto: 'Esse é um recado de teste',
+      de: 'Joana',
+      para: 'João',
+      lido: false,
+      data: new Date(),
+    },
+  ];
+
+  findAll() {
+    return this.recados;
+  }
+
+  findOne(id: string) {
+    return this.recados.find((item) => item.id === +id);
+  }
+
+  create(body: any) {
+    this.lastId++;
+    const id = this.lastId;
+    const newRecado = {
+      id,
+      ...body,
+    };
+    this.recados.push(newRecado);
+
+    return newRecado;
+  }
+
+  update(id: any, body: any) {
+    const recadoExistenteIndex = this.recados.findIndex(
+      (item) => item.id === +id,
+    );
+
+    if (recadoExistenteIndex >= 0) {
+      const recadoExistente = this.recados[recadoExistenteIndex];
+
+      this.recados[recadoExistenteIndex] = {
+        ...recadoExistente,
+        ...body,
+      };
+      return recadoExistente;
+    }
+  }
+
+  remove(id: any) {
+    const recadoExistenteIndex = this.recados.findIndex(
+      (item) => item.id === +id,
+    );
+
+    if (recadoExistenteIndex >= 0) {
+      this.recados.splice(recadoExistenteIndex, 1);
+    }
   }
 }
