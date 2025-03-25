f19779a506a4237e6cc083804aad33e17d361bc8
Fagner Dev <fagnersro.dev@gmail.com>
2025-03-25 11:30:20 -0300

    Person CRUD: creating findAll, findOne (Read) and remove (Delete)

--- src/pessoas/pessoas.service.ts
+++ src/pessoas/pessoas.service.ts
@@ -1,4 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreatePessoaDto } from './dto/create-pessoa.dto';
 import { UpdatePessoaDto } from './dto/update-pessoa.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -32,8 +36,14 @@ export class PessoasService {
     }
   }
 
-  findAll() {
-    return `This action returns all pessoas`;
+  async findAll() {
+    const pessoas = await this.pessoaRepository.find({
+      order: {
+        id: 'desc',
+      },
+    });
+
+    return pessoas;
   }
 
   findOne(id: number) {
@@ -44,7 +54,13 @@ export class PessoasService {
     return `This action updates a #${id} pessoa`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pessoa`;
+  async remove(id: number) {
+    const pessoa = await this.pessoaRepository.findOneBy({ id });
+
+    if (!pessoa) {
+      throw new NotFoundException('Pessoa não encontrada');
+    }
+
+    return this.pessoaRepository.remove(pessoa);
   }
 }
